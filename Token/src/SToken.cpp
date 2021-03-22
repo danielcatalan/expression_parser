42cdf9c38e10d534a98fa6776be968bf5e4fb367
Daniel Catalan <dancatalan415@gmail.com>
2021-03-22 16:13:48 -0700

    dev on GetOperatorMarkers

--- Token/src/SToken.cpp
+++ Token/src/SToken.cpp
@@ -1,8 +1,29 @@
 #include "SToken.hpp"
+#include <array>
 
-std::vector<int> GetOperatorMarkers(const char* str)
+std::vector<int> GetOperatorMarkers(const char* cstr)
 {
     std::vector<int> positions;
+    std::array<std::string, 2> delims = {"+", "-"};
+    std::string str = cstr;
+    
+    auto start = 0;
+    auto pos = 0;
+
+    for(auto& d : delims)
+    {
+        while (true)
+        {
+            pos = str.find(d, start);
+
+            if(pos == std::string::npos)
+            {
+                break;
+            }
+            positions.push_back(pos);
+            start = pos+1;
+        }
+    }
 
 
     return positions;
