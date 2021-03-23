3fbbc8337b7d6657403eb32dd0baed3377b3a76f
Daniel Catalan <dancatalan415@gmail.com>
2021-03-22 21:51:10 -0700

    add functions

--- Token/src/OperatorToken.cpp
+++ Token/src/OperatorToken.cpp
@@ -1,13 +1,40 @@
 #include "OperatorToken.hpp"
+#include <cmath>
+
+double None(double l, double r)
+{
+    return NAN;
+}
 
 double Add(double l, double r)
 {
     return l+r;
 }
 
+double Sub(double l, double r)
+{
+    return l-r;
+}
+
+auto GetFunc(OperatorType type)
+{
+    switch (type)
+    {
+    case OperatorType::Add:
+        return Add;
+        break;
+    case OperatorType::Subtract:
+        return Sub;
+        break;
+    default:
+        return None;
+        break;
+    }
+}
+
 OperatorToken::OperatorToken(OperatorType type) : op(type), IToken(TokenType::Operator)
 {
-    this->func = Add;
+    this->func = GetFunc(type);
 }
 
 bool OperatorToken::IsNumber()
