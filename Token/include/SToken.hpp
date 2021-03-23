dde58b6265dd34a662fa11a1705aae5ad80699d1
Daniel Catalan <dancatalan415@gmail.com>
2021-03-22 21:50:38 -0700

    add descriptions

--- Token/include/SToken.hpp
+++ Token/include/SToken.hpp
@@ -3,6 +3,13 @@
 #include <vector>
 #include <string>
 
+/**
+ * @brief Create String Tokens based on expression. If 
+ * expression is "4+3.1", output would be ["4" "+", "3.1"]
+ * 
+ * @param str Expression to be parsed
+ * @return std::vector<std::string> Vector of strings.
+ */
 std::vector<std::string> STokens(const char* str);
 
 std::vector<int> GetOperatorMarkers(const char* str);
